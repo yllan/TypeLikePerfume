d2e55652c4bc5cb809e7d7db35ab79c2accd7710
Yung-Luen Lan <yllan@me.com>
2014-06-06 00:20:41 +0800

    Initial Commit

---
+++ TypeLikePerfume/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TypeLikePerfume
+//
+//  Created by Yung-Luen Lan on 6/6/14.
+//  Copyright (c) 2014 Yung-Luen Lan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
